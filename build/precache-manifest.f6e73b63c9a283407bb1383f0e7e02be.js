21e04012db1e0436746b8483526f68264a605b09
chrissyalbert <chrissy_albert@yahoo.com>
2020-07-10 15:40:00 -0700

    deploy pomodoro github page

---
+++ build/precache-manifest.f6e73b63c9a283407bb1383f0e7e02be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db6b2cba86f594fd825c3a0a92bfdbc7",
+    "url": "/pomodoro/index.html"
+  },
+  {
+    "revision": "73ff9f7eb6f6099ff096",
+    "url": "/pomodoro/static/css/main.989a2e07.chunk.css"
+  },
+  {
+    "revision": "72b97299d96f65b8a55f",
+    "url": "/pomodoro/static/js/2.2612dac3.chunk.js"
+  },
+  {
+    "revision": "55fc676fdc7ca9e494c0eed4405b4601",
+    "url": "/pomodoro/static/js/2.2612dac3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73ff9f7eb6f6099ff096",
+    "url": "/pomodoro/static/js/main.9bce5180.chunk.js"
+  },
+  {
+    "revision": "ee3590aa5d45d2dacf12",
+    "url": "/pomodoro/static/js/runtime-main.f3456f13.js"
+  }
+]);
